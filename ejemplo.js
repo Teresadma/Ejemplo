e76740ccd802e38a156e54db3ad6ac68d282f62f
Teresa de Miguel <teresa.demiguel.alberti@gmail.com>
2023-05-02 20:12:55 +0200

    Primer commit

---
+++ ejemplo.js
@@ -0,0 +1,15 @@
+function sum(num1,num2=0){
+    return num1 + num2
+};
+function subs(num1,num2=0){
+    return num1 - num2
+};
+function mult(num1,num2=0){
+    return num1*num2
+};
+function div(num1,num2=0){
+    return num1/num2
+}
+function cuad(num1){
+    return num1*num1
+}
